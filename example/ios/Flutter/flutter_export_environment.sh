a184db863acd21cdbfdf7c4163c024d952c77d04
Quentin Le Guennec <quentin@londonbytelabs.com>
2023-01-27 15:07:51 -0800

    Initial Commit. Version 4.0.0+1 of states_rebuilder, unmodified.

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/run/media/gafatahda/91cd3798-6fc4-4d7e-bb5c-33f198138881/mafatahnj/flutter"
+export "FLUTTER_APPLICATION_PATH=/run/media/gafatahda/5195A8876A200EC0/Repository/states_rebuilder/states_rebuilder_package/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
